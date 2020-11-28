b7a2444c54ece34a5ab80a7d0ecc57ae8d6a4300
AYENI OREJAH <68875028+OREJAH@users.noreply.github.com>
2020-11-28 08:28:12 +0100

    Update train.py
    okay now

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
-return x_df,y_df
+    return x_df,y_df
     
     # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
