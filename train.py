7d590667a8fff6546b2827d34b95922d535a8c92
AYENI OREJAH <68875028+OREJAH@users.noreply.github.com>
2020-11-28 08:22:20 +0100

    Update train.py
    almost done

--- train.py
+++ train.py
@@ -10,21 +10,6 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-src= "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-ds = TabularDatasetFactory.from_delimited_files(path=src)
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test=train_test_split(x, y, train_size=0.8, test_size=0.2, random_state=42)
-
-run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -52,7 +37,24 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
+return x_df,y_df
+    
+    # TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
+src= "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+ds = TabularDatasetFactory.from_delimited_files(path=src)
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test=train_test_split(x, y, train_size=0.8, test_size=0.2, random_state=42)
+
+run = Run.get_context()
+    
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
